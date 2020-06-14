07c485c66dc85336e1f5995d18be4d451c33c5a5
gulzar <ali.gulzar@tuni.fi>
2020-06-15 00:07:19 +0500

    Login page layout, notification when log in

--- src/react/App.js
+++ src/react/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import Lottie from 'react-lottie';
-import animationData from './assets/animations/login_books.json';
+import animationData from './assets/animations/work.json';
 import WebFont from 'webfontloader';
+import { Form, Input, Button, notification } from 'antd';
 
-const defaultOptions = {
-  loop: true,
-  autoplay: true, 
-  animationData: animationData,
-};
-
+// Load custom fonts
 WebFont.load({
   google: {
     families: ['Indie Flower', 'MuseoModerno:300,400,700', 'Kaushan Script']
@@ -18,21 +14,99 @@ WebFont.load({
   }
 })
 
+// Options for animations
+const defaultOptions = {
+  loop: true,
+  autoplay: true, 
+  animationData: animationData,
+};
+
+// Layout for login form
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowHeight: window.innerHeight / 3.5,
+      disable: false
+    }
+  }
+
+  onFinish = values => {
+    this.setState({disable: true})
+    notification['success']({
+      message: 'Successfull',
+      description: `Welcome ${values.username} to Ultra Strength Shop.`
+    });
+    this.setState({disable: false})
+  };
+
+  onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
 
   renderLoadingAnimation = () => {
     return (
-      <Lottie options={defaultOptions}
-        height={400}
-        width={400}
-      />
+      <div style={{display: 'flex'}}>
+        <Lottie options={defaultOptions}
+          height={650}
+          width={650}
+        />
+      </div>
     )
   }
 
   renderLoginForm = () => {
+
+    var {windowHeight, disable} = this.state;
+
     return (
-      <div style={{display: 'flex', marginLeft: 50}}>
+      <div style={{display: 'flex', flexDirection: 'column', marginTop: windowHeight }}>
         <h3>Login to your account</h3>
+        <Form
+          {...layout}
+          name="login-form"
+          initialValues={{ remember: true }}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit" disabled={disable}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     )
   }
@@ -42,9 +116,10 @@ export default class App extends Component {
     return(
       <div>
         <div style={{display: 'flex', justifyContent: 'center', fontFamily: 'Kaushan Script'}}>
-          <h1>Ultra Strength Shop</h1>
+          <h1 style={{marginTop: 20, fontSize: 40}}>Ultra Strength Shop</h1>
         </div>
-        <div style={{display: 'flex'}}>
+        <div style={{display: 'flex', justifyContent: 'space-around'}}>
+          {this.renderLoginForm()}
           {this.renderLoadingAnimation()}
         </div>
       </div>
