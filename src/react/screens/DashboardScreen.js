8352ea9efd11807d0cc0292b6ec1e97a2b6510d2
gulzar <ali.gulzar@tuni.fi>
2020-06-18 18:10:28 +0500

    Project done

--- src/react/screens/DashboardScreen.js
+++ src/react/screens/DashboardScreen.js
@@ -8,6 +8,7 @@ export default function DashboardScreen () {
 
   const [cash, setCash] = useState(null);
   const [sales, setSales] = useState(null);
+  const [expenses, setExpenses] = useState(null);
 
   useEffect(() => {
     firebase.database().ref('cash/total').once('value', async function(snapshot) {
@@ -20,14 +21,19 @@ export default function DashboardScreen () {
         setSales(snapshot.val().value)
       } else setSales(0)
     })
+    firebase.database().ref('expenses/total').once('value', async function(snapshot) {
+      if(snapshot.val()) {
+        setExpenses(snapshot.val().value)
+      } else setExpenses(0)
+    })
   },[])
 
   return (
     <div>
       <div style={{display: 'flex', marginBottom: 50}}>
-        <Card colorValue="card-profit" title="Profit" value="0"/>
+        <Card colorValue="card-profit" title="Cash" value={cash}/>
         <Card colorValue="card-sales" title="Sales" value={sales}/>
-        <Card colorValue="card-expenses" title="Cash" value={cash}/>
+        <Card colorValue="card-expenses" title="Expenses" value={expenses}/>
       </div>
       <Graph/>
     </div>
